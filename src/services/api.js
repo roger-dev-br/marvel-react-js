392d85ece623788869730df1ac53224d6dd8af5c
Roger Medeiros <rogermedeiros@outlook.com>
2022-03-16 21:36:36 -0300

    kye

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
     baseURL: 'http://gateway.marvel.com/v1/public',
 });
 
-const marvelKey = `ts=1631572374297&apikey=6b4f4a0506338bc9c00889db929533d7&hash=ce9ca3b84c5d534a03c5c7c6fbc7eff0`;
+const marvelKey = `ts=1631572374297&apikey=6b4f4a0506338bc9c00889db929533d7&hash=ce9ca3b84c5d534a03c5c7c6fbc7eff`;
 
 export {
     api,
